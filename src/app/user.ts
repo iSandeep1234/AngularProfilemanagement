117714e32b14266bc8f1c5a004a05890fcaf2123
iSandeep1234 <sandeeppaswan.pcsglobal@gmail.com>
2021-05-19 12:44:08 -0700

    third commit

--- src/app/user.ts
+++ src/app/user.ts
@@ -5,8 +5,11 @@ export class User {
     dateOfBirth : Date;
     profilePic : File;
     carrierobjective : string;
-    
-    
+    projectDetails :string;
+    personalDetails :string;
+    technicalSkill : string;
+    educationDetails : string;
+    experiencedDetails : string;
 
-     constructor(userId : number, userName : string, dateOfBirth : Date, profilePic : File, carrierobjective : string){}
+     constructor(userId : number, userName : string, dateOfBirth : Date, profilePic : File, carrierobjective : string,projectDetails : String,personalDetails : string,technicalSkill : string,educationDetails : string,experiencedDetails : string){}
 }
